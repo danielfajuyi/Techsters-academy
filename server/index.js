aa9ba4b19395ef5a7da414376e66ab72dbce28d0
Daniel Fajuyi <ayomidedaniels121@gmail.com>
2025-03-16 11:04:21 +0100

    versel config

--- server/index.js
+++ server/index.js
@@ -15,9 +15,5 @@ app.use(cors());
 // Routes
 app.get("/", (req, res) => res.send("Api Working"));
 app.post("/clerk", express.json(), clerkWebhooks);
-// Port
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+export default app;
