f9228ee8b5977cd224a539bb92ea27fd9aa4e8d7
Daniel Fajuyi <ayomidedaniels121@gmail.com>
2025-03-16 11:10:59 +0100

    versel ccnfig

--- server/index.js
+++ server/index.js
@@ -4,16 +4,20 @@ import "dotenv/config.js";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
 
-// Initialize Express
 const app = express();
 
 // Connect to database
-await connectDB();
+(async () => {
+  await connectDB();
+})();
+
 // Middlewares
 app.use(cors());
+app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => res.send("Api Working"));
-app.post("/clerk", express.json(), clerkWebhooks);
+app.get("/", (req, res) => res.send("API Working"));
+app.post("/clerk", clerkWebhooks);
 
+// Export the app (Vercel handles the server)
 export default app;
