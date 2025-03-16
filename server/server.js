4c21a704cb68ec4521245a5718a47edb28be115f
Daniel Fajuyi <ayomidedaniels121@gmail.com>
2025-03-16 09:25:09 +0100

    modified server.js

--- server/server.js
+++ server/server.js
@@ -21,3 +21,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
+
+// Export the Express app for Vercel
+export default app;
