09625c7ecf1918b99d1d21dfde7f343d7c271c05
kachi <agohdennis99@gmail.com>
2025-03-18 22:58:49 +0100

    deleted unncessaries

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,13 +1,7 @@
 import mongoose from "mongoose";
 
-// Connect to the MongoDB database
 
-const connectDB = async () => {
-  mongoose.connection.on("connected", () =>
-    console.log("Database is Connected")
-  );
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/techsters-academy`);
-};
+
 
 export default connectDB;
--- server/routes/educatorRoutes.js
+++ server/routes/educatorRoutes.js
@@ -1,9 +1,3 @@
 import express from "express";
 import { updateRoleToEducator } from "../controllers/educatorController.js";
 
-const educatorRouter = express.Router();
-
-//Add Educator Role
-educatorRouter.get("/update-role", updateRoleToEducator);
-
-export default educatorRouter;
