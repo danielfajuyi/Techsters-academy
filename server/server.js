d5b9a6111cd56b23b39eecaa85642ba94dd7d1c7
kachi <agohdennis99@gmail.com>
2025-03-18 22:53:45 +0100

    deleted server.js

--- server/server.js
+++ server/server.js
@@ -8,25 +8,6 @@ import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
-// Connect to database
-await connectDB();
-
-// Middlewares
-app.use(cors());
-app.use(clerkMiddleware());
-app.use(express.json());
-
-// Routes
-app.get("/", (req, res) => res.send("API Working"));
-app.post("/clerk", clerkWebhooks);
-app.use("/api.educator", express.json(), educatorRouter);
-// For local development
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-  });
-}
 
 // Export the Express app for Vercel
 export default app;
