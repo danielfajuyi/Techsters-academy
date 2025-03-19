40228e9843fb3265f4a36a919bb38a4c75d7a59c
kachi <agohdennis99@gmail.com>
2025-03-19 13:38:39 +0100

    database connected and vercel json added

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
+//connect to the MongoDB database
 
+const connectDB = async ()=>{
+    mongoose.connection.on('connected', ()=> console.log('databse Connected'))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+}
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config.js";
+import 'dotenv/config'
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
-import educatorRouter from "./routes/educatorRoutes.js";
-import { clerkMiddleware } from "@clerk/express";
 
+
+// initialize Experess
 const app = express();
 
+//connect to database
+await connectDB()
 
-// Export the Express app for Vercel
-export default app;
+//middlewares
+app.use(cors())
+
+
+//Routes
+app.get('/',(req, res)=> res.send("API Working"))
+app.post('/clerk', express.json(), clerkWebhooks )
+
+//port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+})
+
+
