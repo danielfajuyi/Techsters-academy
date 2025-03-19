8f5edf5586b337e3774279c352bf1a94f82b052a
kachi <agohdennis99@gmail.com>
2025-03-19 21:40:12 +0100

    updated educatorRoutes,serverjs,cloudinary,multer,webhooks and middleware

--- server/server.js
+++ server/server.js
@@ -3,21 +3,27 @@ import cors from "cors";
 import 'dotenv/config'
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import educatorRouter from "./routes/educatorRoutes.js";
+import { clerkMiddleware } from "@clerk/express";
+import connectCloudinary from "./configs/cloudinary.js";
 
 
 // initialize Experess
 const app = express();
 
 //connect to database
-await connectDB()
+await connectDB();
+await connectCloudinary(); 
 
 //middlewares
 app.use(cors())
+app.use(clerkMiddleware())
 
 
 //Routes
 app.get('/',(req, res)=> res.send("API Working"))
 app.post('/clerk', express.json(), clerkWebhooks )
+app.use('/api/educator',express.json(), educatorRouter )
 
 //port
 const PORT = process.env.PORT || 5000
