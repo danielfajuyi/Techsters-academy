ead73c7cab042d07fd676e4118cd06f02b4c9fd3
kachi <agohdennis99@gmail.com>
2025-03-27 09:51:18 +0100

    corrrected user creation

--- server/models/Course.js
+++ server/models/Course.js
@@ -34,7 +34,7 @@ const courseSchema = new mongoose.Schema(
     courseRating: [
       { userId: { type: String }, rating: { type: Number, min: 1, max: 5 } },
     ],
-    educator: { type: String, ref: "user", required: true },
+    educator: { type: String, ref: "User", required: true },
     enrolledStudents: [{ type: String, ref: "User" }],
   },
   { timestamos: true, minimize: false }
