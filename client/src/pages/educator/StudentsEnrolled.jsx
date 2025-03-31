a6d6dcbc37d2cec5e949440b959e2e2e165fabdf
Daniel Fajuyi <ayomidedaniels121@gmail.com>
2025-03-31 18:40:29 +0100

    v1 techsters-academy completed

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -1,19 +1,39 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { dummyStudentEnrolled } from "../../assets/assets";
 import Loading from "../../components/student/Loading";
+import { AppContext } from "../../context/AppContext";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const StudentsEnrolled = () => {
+  const { backendUrl, isEducator, getToken } = useContext(AppContext);
 
   const [enrolledStudents, setEnrolledStudents] = useState(null);
 
-  const fectchEnrolledStudents = async () =>{
-    setEnrolledStudents(dummyStudentEnrolled)
-  }
+  const fectchEnrolledStudents = async () => {
+    try {
+      const token = await getToken();
+      const { data } = await axios.get(
+        backendUrl + "/api/educator/enrolled-students",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      if (data.success) {
+        setEnrolledStudents(data.enrolledStudents.reverse());
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
-  useEffect(()=>{
-    fectchEnrolledStudents()
-  },[])
-
+  useEffect(() => {
+    if (isEducator) {
+      fectchEnrolledStudents();
+    }
+  }, [isEducator]);
 
   return enrolledStudents ? (
     <div className="min-h-screen flex flex-col items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0">
@@ -24,27 +44,31 @@ const StudentsEnrolled = () => {
               <th className="px-4 py-3 font-semibold text-center hidden sm:table-cell">
                 #
               </th>
-              <th className="px-4 py-3 font-semibold">
-                Student Name
-              </th>
-              <th className="px-4 py-3 font-semibold">
-                Course Title
-              </th>
+              <th className="px-4 py-3 font-semibold">Student Name</th>
+              <th className="px-4 py-3 font-semibold">Course Title</th>
               <th className="px-4 py-3 font-semibold text-center hidden sm:table-cell">
                 Date
               </th>
             </tr>
           </thead>
           <tbody className="text-sm text-gray-500">
-            {enrolledStudents.map((item, index)=> (
+            {enrolledStudents.map((item, index) => (
               <tr key={index} className="border-b border-gray-500/20">
-                <td className="px-4 py-3 text-center hidden sm:table-cell">{index + 1}</td>
+                <td className="px-4 py-3 text-center hidden sm:table-cell">
+                  {index + 1}
+                </td>
                 <td className="md:px-4 px-2 py-3 flex items-center space-x-3">
-                  <img src={item.student.imageUrl} alt="student_profile_image" className="w-9 h-9 rounded-full" />
+                  <img
+                    src={item.student.imageUrl}
+                    alt="student_profile_image"
+                    className="w-9 h-9 rounded-full"
+                  />
                   <span className="truncate">{item.student.name}</span>
                 </td>
                 <td className="px-4 py-3 trunctate">{item.courseTitle}</td>
-                <td className="px-4 py-3 hidden sm:table-cell">{new Date(item.purchaseDate).toLocaleDateString()}</td>
+                <td className="px-4 py-3 hidden sm:table-cell">
+                  {new Date(item.purchaseDate).toLocaleDateString()}
+                </td>
               </tr>
             ))}
           </tbody>
